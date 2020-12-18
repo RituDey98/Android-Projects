b7456c36bd00cde02d298e1c61a8b637b18fa17b
Ritu Dey <deyritu98@gmail.com>
2020-12-18 21:20:25 +0530

    Added location detection and arrayAdapter

--- MemorablePlaces/app/src/main/java/com/ritudey/memorableplaces/MainActivity.java
+++ MemorablePlaces/app/src/main/java/com/ritudey/memorableplaces/MainActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView addPlaces;
     Intent intent;
-
+    ListView myListView;
+    ArrayList<String> myFavPlaces;
 
 
     public  void OnClickAddPlaces(View v)
@@ -26,6 +33,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addPlaces = findViewById(R.id.addTv);
+        myListView=findViewById(R.id.listView);
+
+        myFavPlaces = new ArrayList<String>();
+        myFavPlaces.add("Rik");
+        myFavPlaces.add("Ritu");
+        myFavPlaces.add("Rik");
+        myFavPlaces.add("Ritu");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,myFavPlaces);
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //take to the next activity here
+            }
+        });
 
     }
 }
--- MemorablePlaces/app/src/main/java/com/ritudey/memorableplaces/MapsActivity.java
+++ MemorablePlaces/app/src/main/java/com/ritudey/memorableplaces/MapsActivity.java
@@ -1,8 +1,18 @@
 package com.ritudey.memorableplaces;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,9 +21,51 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback  {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    Location lastKnownLocation;
+
+
+
+
+    public void centerOnMapLocation(Location myLocation, String title) {
+        LatLng userLocation = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+
+        mMap.clear();        //clearing previous markers
+
+        if(!title.equals("Your location"))
+        {
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 10));
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                //lastKnownLocation=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            }
+
+        }
+    }
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +75,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+        //zooming on user location
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                centerOnMapLocation(location,"Your location");
+                Log.i("userloaction", String.valueOf(location));
+            }
+        };
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+            //we have permission
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,35000,0,locationListener);
+//            lastKnownLocation =locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            centerOnMapLocation(lastKnownLocation,"Your location");
+
+        }else{
+
+            //asking for permission
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }
+
+
+
+
     }
+
+
+
+
+
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        mMap.setOnMapLongClickListener(this);
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 
 }
